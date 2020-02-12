6471e86d9841d79efde24283d9bd06832270b448
Phonxay Champasouk <52714374+phonxaychampasouk@users.noreply.github.com>
2020-02-11 18:18:56 -0800

    Complete File

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,23 @@
 var HoneyMakerBee = function() {
+  Bee.call(this);
+
+  this.age = 10;
+  this.job = 'make honey';
+  this.honeyPot = 0;
 };
 
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+HoneyMakerBee.prototype.makeHoney = function() {
+  this.honeyPot++;
+}
+
+// Returns a boolean: An interface to observe whether honey was given.
+HoneyMakerBee.prototype.giveHoney = function() {
+  if (this.honeyPot > 0) {
+    this.honeyPot--;
+    return true;
+  }
+  return false;
+}
